cba0ea6e53a6d34031c73393aab2bf65d2a8cca1
Jose Benavides <josebenavides.dev@gmail.com>
2022-05-29 17:38:39 -0500

    Tipos de datos

---
+++ 01_tipos_datos.py
@@ -0,0 +1,11 @@
+cadena = "Hola Mundo!"
+entero = -21
+flotante = 10.5
+verdadero = True
+falso = False
+
+print(type(cadena))  # <class 'str'>
+print(type(entero))  # <class 'int'>
+print(type(flotante))  # <class 'float'>
+print(type(verdadero))  # <class 'bool'>
+print(type(falso))  # <class 'bool'>
