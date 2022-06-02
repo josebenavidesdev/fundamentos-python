781562edafd6e2ce8b9e979bb3a65edf6227924b
Jose Benavides <josebenavides.dev@gmail.com>
2022-06-02 16:54:58 -0500

    Cadenas

---
+++ 02_cadenas.py
@@ -0,0 +1,20 @@
+cadena = "Python!"
+
+# INDEXACION
+print(len(cadena))  # 7
+print(cadena[0])  # P
+print(cadena[-1])  # !
+
+# REBANADO
+print(cadena[0:])  # Python!
+print(cadena[0:len(cadena)])  # Python!
+print(cadena[:])  # Python!
+print(cadena[:2])  # Py
+
+# AGREGANDO SALTO
+print("pryutbhyo!"[1::2])  # ruby!
+
+# METODOS
+print("hola python".capitalize())  # Hola python
+print("foo".upper())  # FOO
+print("BAR".lower())  # bar
