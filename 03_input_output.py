353192d9d4a83c38947375a60135566409e6ef51
Jose Benavides <josebenavides.dev@gmail.com>
2022-06-08 22:34:04 -0500

    input de datos, valores Truthy y Falsy

---
+++ 03_input_output.py
@@ -0,0 +1,23 @@
+numero = float(input("Ingrese un número: "))
+
+print(numero, type(numero))  # 10.0 <class 'float'>
+
+print(int("20"))  # 20
+print(str(30))  # 30
+print(bool("a"))  # True
+print(bool(""))  # False
+print(bool(" "))  # True
+print(bool(0))  # False
+print(bool(-15))  # True
+
+""" valores Falsy
+0
+0.0
+''
+""
+[]
+()
+{}
+set()
+range(0)
+"""
