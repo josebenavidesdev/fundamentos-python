076a0ba7fc48b00190cbfe4a255b3a4a45d90e5a
Jose Benavides <josebenavides.dev@gmail.com>
2022-07-01 02:59:07 -0500

    Programacion orientada a objetos

---
+++ 16_poo.py
@@ -0,0 +1,22 @@
+# self -> instancia actual
+
+class CuentaBancaria:
+    def __init__(self, num_cuenta, nombre_titular, balance):
+        self.num_cuenta = num_cuenta
+        self.nombre_titular = nombre_titular
+        self.balance = balance
+
+    # muestra el balance actual
+    def mostrar_balance(self):
+        print(self.balance)
+
+    # incrementa el balance actual
+    def depositar(self, monto):
+        if monto > 0:
+            self.balance += monto
+
+mi_cuenta = CuentaBancaria("105-356-643", "Nora Smith", 5600)
+
+mi_cuenta.mostrar_balance()  # 5600
+mi_cuenta.depositar(400)
+mi_cuenta.mostrar_balance()  # 6000
